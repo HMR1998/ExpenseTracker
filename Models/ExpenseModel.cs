58793b61c7850ee4409cb4a3dc744376f62f2367
Humza <humzahnmr@gmail.com>
2025-05-07 21:38:25 +0100

    Added basic Expense model

---
+++ Models/ExpenseModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ExpenseTracker.Models
+{
+    public class ExpenseModel
+    {
+        public int Id { get; set; }
+        public string ExpenseTitle { get; set; } = string.Empty;
+        public string ExpenseDescription { get; set; } = string.Empty;
+        public string ExpenseType { get; set; } = string.Empty;
+        public DateTime ExpenseDate { get; set; } = DateTime.Now;
+        public int ExpenseCost { get; set; }
+
+    }
+}
