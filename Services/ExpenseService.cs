8db328932999229f9f93fd1d592506a3920973b9
Humza <humzahnmr@gmail.com>
2025-05-09 18:37:09 +0100

    Added ExpenseService with GetAll and CreateExpense methods

---
+++ Services/ExpenseService.cs
@@ -0,0 +1,26 @@
+﻿using ExpenseTracker.Data;
+using ExpenseTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Services
+{
+    public class ExpenseService
+    {
+        private readonly DataContext _dataContext;
+
+        public ExpenseService(DataContext dataContext) =>
+            _dataContext = dataContext;
+
+        public async Task<IEnumerable<ExpenseModel?>> GetAllExpenses()
+        {
+            return await _dataContext.ExpenseModels.ToListAsync();
+        }
+
+        public async Task<ExpenseModel> AddExpense(ExpenseModel expenseModel)
+        {
+            var expense = await _dataContext.AddAsync(expenseModel);
+            await _dataContext.SaveChangesAsync();
+            return expense.Entity;
+        }
+    }
+}
